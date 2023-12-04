7b09a6bf62c5ed7a185fdbbe6eaf410fcaa1d1b4
root <root@DESKTOP-K4B994M>
2023-12-03 20:32:13 -0800

    third commit

--- PuppyBowl.js
+++ PuppyBowl.js
@@ -1,12 +1,12 @@
 let puppies = []
 const allPuppies = document.querySelector('#allPuppies')
 const singPup = document.querySelector('#singPup')
+const allPupPlayers = document.querySelector('#allPupPlayers')
+
 
 window.addEventListener("hashchange", () => { //to let user click on each puppy and view it , each time the # changes the eventlistener will update the page
     render()
 })
-
-
 
 async function getAllPuppies() {
     const response = await fetch("https://fsa-puppy-bowl.herokuapp.com/api/2310/players")
@@ -16,34 +16,39 @@ async function getAllPuppies() {
     render()
 } 
 
-async function render() {
+function render() {
     const puppyPlayers = puppies.map((pup) =>{
         return`
-        <h3>
-        <br> <img src=${pup.imageUrl} width= 400px; height= 400px> </br> 
-        <br> <a href=#${pup.name} class="pupName"> Name: ${pup.name}</a> 
-        <br><a href=#${pup.breed} class="pupBreed"> Breed: ${pup.breed} </a> 
-        <br><a href=#${pup.status} class="pupStatus"> Status: ${pup.status}</a> 
+        <center><h3>
+        <br> <img src=${pup.imageUrl} width= 400px; height= 400px> </br>
+        <br> <div><a href=#${pup.name} class="pupName"> Name: ${pup.name}</a> </div>
+        <br><a> Breed: ${pup.breed} </a> 
+        <br><a> Status: ${pup.status} </a></center>
        `
     })
     allPuppies.innerHTML = puppyPlayers.join("")
     
     const name = window.location.hash.slice(1)
-    
 
-    const singlePup= puppyPlayers.find((pup)=>{
-    return pup.name ===name
-    
-})
- //console.log(name) console logging the name shows that each time the user clicks the name of the puppy, it updastes the hash, and also is sending a message to the log of the specific puppy name.
+    const singlePup= puppies.find((pup)=> {
+        return pup.name === name
+    }) 
+    if (singlePup){
 
-allPuppies.innerHTML = singPup ? "":"<h1> All Puppy Bowl Players <h1>" +
-/*if(singlePup) {
-    const puppyData = await fetch(singlePup.url)
-    const singlePupData = await puppyData.json()
-    //console.log(singlePupData)
-      }*/
-}
+        singPup.innerHTML = `
+       <center> <h2>
+        <br> <img src=${singlePup.imageUrl} width= 400px; height= 400px> </br> 
+        <br> <a> Name: ${singlePup.name}</a>
+        <br><a> Breed: ${singlePup.breed} </a> 
+        <br><a> Status: ${singlePup.status} </a> 
+        <div> <a class="Return" href =#> Return to Players </a> </div></center>
+
+       `
+    } else{
+        singPup.innerHTML =""
+    }
+
+}     
 getAllPuppies()
 
 
